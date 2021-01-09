cd81d9b679e71d3577b018d36c30b98e30d19341
Nisan Abeywickrama <nisanabeywickrama@gmail.com>
2021-01-09 14:50:46 +0530

    updated firebase admin

--- util/admin.js
+++ util/admin.js
@@ -1,6 +1,6 @@
 //Configuring firebase admin and initialization
 const admin = require("firebase-admin");
-const serviceAccount = require("../config/dev-hackathon-11f94-firebase-adminsdk-h7t6q-fb2a87211a.json");
+const serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
