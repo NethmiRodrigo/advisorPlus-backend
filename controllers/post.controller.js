0f2fd991ffdb5ae3ab2f8c63726cf99b6b9eb3cc
Nisan Abeywickrama <nisanabeywickrama@gmail.com>
2021-01-11 07:07:19 +0530

    fix issues with post controller

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,4 +1,5 @@
-const { Post } = require("../models");
+const { response } = require("express");
+const { Post, User_Profile } = require("../models");
 
 exports.create = (req, res) => {
   if (!req.body) {
@@ -27,54 +28,62 @@ exports.create = (req, res) => {
     });
 };
 exports.findAll = (req, res) => {
-  Post.findAll()
-    .then((data) => {
-      res.send(data);
+
+  try{
+    let posts = await Post.findAll();
+    posts.forEach((item)=>{
+      item.full_name = User_Profile.findByPk(item.user_id).full_name
     })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "ERROR",
-      });
-    });
+    return response.status(200).json({posts});
+  }
+
+  catch(error){
+    response.status(500).json({error});
+  }
 };
 exports.findById = (req, res) => {
   const id = req.params.postId;
-  Post.findByPk(id)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message,
-      });
-    });
+  try{
+  let PostbyID = Post.findByPk(id)
+    PostbyID.full_name = await User_Profile.findByPk(PostbyID.user_id);
+    return response.status(200).json({PostbyID});
+  }
+  catch(error){
+    response.status(500).json({error})
+  }
+  
 };
 exports.findAllByUser = (req, res) => {
-  const user_id = req.query.user_id;
+  const user_id = req.params.userId;
   var condition = user_id ? { user_id: user_id } : null;
-  Post.findAll({ where: condition })
-    .then((data) => {
-      res.send(data);
+  let user =  await User_Profile.findByPk(user_id)
+
+  try{
+    let listPosts = Post.findAll({ where: condition })
+    listPosts.forEach((item) => {
+      item.full_name = user.full_name;
     })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "error occurred while retrieving Posts.",
-      });
-    });
+    return response.status(200).json({listPosts});
+  }
+  catch(error){
+    response.status(500).json({error})
+  }
 };
 
 exports.findAllByPost = (req, res) => {
   const post_id = req.params.post_id;
   var condition = post_id ? { post_id: post_id } : null;
-  Post.findAll({ where: condition })
-    .then((data) => {
-      res.send(data);
+  try{
+    let listPosts = Post.findAll({ where: condition })
+    listPosts.forEach((item)=>{
+      let user = await User_Profile.findByPk(item.user_id);
+      item.full_name = user.full_name;
     })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "error occurred while retrieving Posts.",
-      });
-    });
+    return response.status(200).json({listPosts});
+}
+  catch(error){
+    response.status(500).json({error})
+  }
 };
 
 exports.update = (req, res) => {
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -13,7 +13,7 @@ module.exports = (app) => {
   app.put("/post/:postId", auth("user"), post.update);
 
   //get all  posts of a user
-  app.get("/post", auth(), post.findAllByUser);
+  app.get("/post/:userId", auth(), post.findAllByUser);
 
   app.delete("/post/:postId", auth("user"), post.delete);
 };
